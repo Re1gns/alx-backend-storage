a6c8a50a5724f83a795cb0a1a508b321b7c18278
Re1gns <lreigns12@gmail.com>
2023-10-17 22:13:30 +0100

    Fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
-from pymongo import MongoClient
+"""Script that returns Nginx logs stats stored in MongoDB"""
+import pymongo
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def collection(db: dict) -> int:
+    """Function to retrieve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
 
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    if option:
-        value = mongo_collection.count_documents({"method": option})
-        print(f"\tmethod {option}: {value}")
-    else:
-        result = mongo_collection.count_documents({})
-        print(f"{result} logs")
-        print("Methods:")
-        for method in METHODS:
-            log_stats(mongo_collection, method)
-        status_check = mongo_collection.count_documents({"method": "GET", "path": "/status"})
-        print(f"{status_check} status check")
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
 
-if __name__ == "__main":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    print(f"{collection({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
+
+if __name__ == "__main__":
+    main()
