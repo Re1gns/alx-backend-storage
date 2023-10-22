fff3818e0c74ce2e89a40f6a49aa34cdfddb72e3
Reigns <lreigns12@gmail.com>
2023-10-22 15:04:37 +0100

    Update fix web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-"""
-web cache and tracker
-"""
 import requests
 import redis
 from functools import wraps
@@ -20,16 +16,12 @@ def count_url_access(method):
             return cached_data.decode("utf-8")
 
         count_key = "count:" + url
-        try:
-            res = method(url)
-            html = res.text
-            store.incr(count_key)
-            store.set(cached_key, html)
-            store.expire(cached_key, 10)
-            return html
-        except requests.exceptions.RequestException as e:
-            # Handle the exception (e.g., log or re-raise) here
-            raise e
+        html = method(url)
+
+        store.incr(count_key)  # Increment the count
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
 
     return wrapper
 
@@ -38,5 +30,26 @@ def count_url_access(method):
 def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
-    res.raise_for_status()  # Raise an exception for non-2xx responses
-    return res
+    return res.text
+
+
+# Function to get the access count for a URL
+def get_url_access_count(url: str) -> int:
+    count_key = "count:" + url
+    count = store.get(count_key)
+    if count:
+        return int(count)
+    return 0
+
+# Function to remove cached data for a URL
+def remove_cached_data(url: str):
+    cached_key = "cached:" + url
+    store.delete(cached_key)
+
+# Example usage:
+url = "http://google.com"
+content = get_page(url)
+access_count = get_url_access_count(url)
+print(f"Content for {url}: {content}")
+print(f"Access count for {url}: {access_count}")
+remove_cached_data(url)  # Remove cached data
